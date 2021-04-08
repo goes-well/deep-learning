0a66dc550bdddd7baa7cd798d361af23254818e1
Sam.Sui <suiyumeng@aliyun.com>
2021-04-08 21:14:17 +0800

    Create model_load_cnn_mnist.py

---
+++ model_load_cnn_mnist.py
@@ -0,0 +1,37 @@
+import os
+#os.environ["TF_CPP_MIN_LOG_LEVEL"] = '1' # 默认，显示所有信息 
+#os.environ["TF_CPP_MIN_LOG_LEVEL"] = '2' # 只显示 warning 和 Error 
+#os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3' # 只显示 Error
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+print(tf.version.VERSION)
+import numpy as np
+
+#数据获取及预处理： tf.keras.datasets
+class MNISTLoader():
+    def __init__(self):
+        mnist = tf.keras.datasets.mnist
+        (self.train_data, self.train_label), (self.test_data, self.test_label) = mnist.load_data()
+        # MNIST中的图像默认为uint8（0-255的数字）。以下代码将其归一化到0-1之间的浮点数，并在最后增加一维作为颜色通道
+        self.train_data = np.expand_dims(self.train_data.astype(np.float32) / 255.0, axis=-1)      # [60000, 28, 28, 1]
+        self.test_data = np.expand_dims(self.test_data.astype(np.float32) / 255.0, axis=-1)        # [10000, 28, 28, 1]
+        self.train_label = self.train_label.astype(np.int32)    # [60000]
+        self.test_label = self.test_label.astype(np.int32)      # [10000]
+        self.num_train_data, self.num_test_data = self.train_data.shape[0], self.test_data.shape[0]
+
+    def get_batch(self, batch_size):
+        # 从数据集中随机取出batch_size个元素并返回
+        index = np.random.randint(0, self.num_train_data, batch_size)
+        return self.train_data[index, :], self.train_label[index]
+
+batch_size = 50
+
+model = tf.saved_model.load("cnn_mnist")
+data_loader = MNISTLoader()
+sparse_categorical_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+num_batches = int(data_loader.num_test_data // batch_size)
+for batch_index in range(num_batches):
+    start_index, end_index = batch_index * batch_size, (batch_index + 1) * batch_size
+    y_pred = model(data_loader.test_data[start_index: end_index])
+    sparse_categorical_accuracy.update_state(y_true=data_loader.test_label[start_index: end_index], y_pred=y_pred)
+print("test accuracy: %f" % sparse_categorical_accuracy.result())
